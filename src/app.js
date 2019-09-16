50f29c430478a0518c42c2a0cfcdedc976598fd9
Rachael Shaw <rachaelleeshaw@gmail.com>
2019-09-16 15:13:23 -0500

    first deploy

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 
+
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
@@ -15,9 +16,10 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req, res) => {
-	res.send('Hello, world!')
-})
+app.get('/api/', (req, res) => {
+   res.json({ok:true});
+ });
+
 
 app.use(function errorHandler(error, req, res, next) {
 	let response
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
- 	PORT: process.env.PORT || 8000,
+ 	PORT: process.env.PORT || 3000,
 	NODE_ENV: process.env.NODE_ENV || 'development',
 }
