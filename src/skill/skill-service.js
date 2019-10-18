325b2bac776de9dfc86efe136debe6b3a7b15b92
Rachael Shaw <rachaelleeshaw@gmail.com>
2019-10-18 11:39:47 -0500

    Skill post with name and altname working

--- src/skill/skill-service.js
+++ src/skill/skill-service.js
@@ -15,7 +15,7 @@ const SkillService = {
 
 	getSkills(knex, queryParams) {
 		console.log("** ** ** ", queryParams)
-		newParams = validate(queryParams, ['action', 'age', 'apparatus', 'level', 'class', 'level', 'name'])		
+		newParams = validate(queryParams, ['action', 'age', 'apparatus', 'level', 'class', 'name'])		
 		let query = knex
 			.select('all_skills.*')
 			.from('all_skills')
@@ -48,37 +48,45 @@ const SkillService = {
 		//validate data(ids) from client, use promise, if exist, oK, if not-throw an error
 			.transaction(function(trx) {
 				//insert the primary name into the name table
+				let skillid
 				return trx ('name')
 				.insert({name: skillFields.primaryname})
 				.returning('id')
 				.then(function(primaryNameId){
 					//insert the skillfields into the skill table, including the primary_name_id
 					return trx
-					.insert({ primary_name_id: primaryNameId[0] })
-					.into('skill')
-					.returning( 'id' )
-					})
-					.then(function(skill_id) {
-						console.log('skill fields', skillFields.alt_names)
-						//return trx
-							const namesToInsert = Object.keys(skillFields.alt_names).map(name => 
-  								({ name: skillFields.alt_names, skill_id: skill_id[0] })); 
-								console.log('skill_id', skill_id[0])
-								console.log('namesToInsert', namesToInsert)
-							trx.insert({ namesToInsert })
-								.into('name')
-							})
-						return skill_id
-						.then(function(skill_id) {
-							delete skillFields.alt_names
-							delete skillFields.primary_name_id
-							return trx('skill')
-							.where('id', skill_id)
-							.update( skillFields )
-							})
-							// .then(trx.commit)
-							// .catch(trx.rollback);
+						.insert({ primary_name_id: primaryNameId[0] })
+						.into('skill')
+						.returning( 'id' )
 				})
+						.then(function(skill_id) {
+							skillid = skill_id
+							return trx('name')
+								.where({ name: skillFields.primaryname })
+								.update({ skill_id: skillid[0] })
+						})
+								.then( () => {
+									const namesToInsert = skillFields.alt_names.map(name => {
+										return {name: name, skill_id: skillid[0]}
+									})
+									return trx
+										.insert( namesToInsert )
+										.into('name')
+								})
+									//return skillid
+									
+										.then( () => {
+											console.log('*********************skillid', skillid)
+											console.log('skillFields', skillFields)
+											delete skillFields.alt_names
+											delete skillFields.primaryname
+											return trx('skill')
+											.where({ id: skillid[0] })
+											.update( skillFields )
+										})
+										// .then(trx.commit)
+										// .catch(trx.rollback);
+			})
 	},
 }
 
