e938f383820f0eb46ac30aeeb170605062eecf35
Rachael Shaw <rachaelleeshaw@gmail.com>
2019-10-22 16:51:58 -0500

    added CORS

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 
 module.exports = {
-  "migrationsDirectory": "migrations",
-  "driver": "pg",
-  "connectionString": (process.env.NODE_ENV === 'test')
-  	? process.env.TEST_DATABASE_URL
-	: process.env.DATABASE_URL,
-  "host": process.env.MIGRATION_DB_HOST,
-  "port": process.env.MIGRATION_DB_PORT,
-  "database": process.env.MIGRATION_DB_NAME,
-  "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
+ 	"migrationsDirectory": "migrations",
+ 	"driver": "pg",
+ 	"connectionString": (process.env.NODE_ENV === 'test')
+ 		? process.env.TEST_DATABASE_URL
+		: process.env.DATABASE_URL,
+	"ssl": !!process.env.SSL,
+ 	"host": process.env.MIGRATION_DB_HOST,
+ 	"port": process.env.MIGRATION_DB_PORT,
+ 	"database": process.env.MIGRATION_DB_NAME,
+ 	"username": process.env.MIGRATION_DB_USER,
+ 	"password": process.env.MIGRATION_DB_PASS
 }
--- src/app.js
+++ src/app.js
@@ -29,9 +29,9 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
+    cors(
+        //origin: CLIENT_ORIGIN
+    )
 );
 
 app.use('/api/auth', authRouter)
